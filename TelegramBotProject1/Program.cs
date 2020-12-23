33a3a5e32890801cf3b46c97054dc52b18a892f8
Leunama-g <47783627+Leunama-g@users.noreply.github.com>
2020-12-23 11:07:51 +0300

    created the telegram bot client object and added the on message even

--- TelegramBotProject1/Program.cs
+++ TelegramBotProject1/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TelegramBotProject1
 {
@@ -7,9 +11,44 @@ namespace TelegramBotProject1
         // the variable holds the generated number
         // *** it's a string so that it can store the numbers like 0123 ***
         static string Gen_num;
+
+        static TelegramBotClient bot = new TelegramBotClient("1487222844:AAFs5T_Wl_xiLkvkxPMAQCXoTmD7caA2pjc");
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+            bot.StartReceiving();
+            bot.OnMessage += Bot_OnMessage;
+
+            Console.ReadLine();
+        }
+
+        private static async void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            string welcomeMessage1 = "<b>Welcome.</b>";
+            string welcomeMessage2 = "This is the @IDposGameBot. Use the buttons below to navigate through the bot. If you need help you can also use /help to get more info.";
+
+            if (e.Message.Text == "/start")
+            {
+                //bot.DeleteMessageAsync("@IDposGameBot", e.Message.MessageId);
+                Message message = await bot.SendTextMessageAsync(
+                chatId: e.Message.Chat, // or a chat id: 123456789
+                text: (welcomeMessage1 + welcomeMessage2),
+                parseMode: ParseMode.Html,
+                disableNotification: true,
+                replyToMessageId: e.Message.MessageId,
+                replyMarkup: new InlineKeyboardMarkup(new InlineKeyboardButton[][]
+                                                        {
+                                                            new [] { new InlineKeyboardButton() { Text = "btn 1"} }, // buttons in row 1
+                                                            new [] { new InlineKeyboardButton() { Text = "btn 2"} }, // buttons in row 2
+                                                            new [] { new InlineKeyboardButton() { Text = "btn 3"} }// buttons in row 3
+                                                        }
+                                                      )
+                );
+            }
+
+
+
         }
 
         static private void GenerateNumber()
