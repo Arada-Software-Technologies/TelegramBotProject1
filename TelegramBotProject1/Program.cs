a67f316db7786038aa20d42584754893063390a4
Leunama-g <47783627+Leunama-g@users.noreply.github.com>
2020-12-23 10:52:50 +0300

    Add project files.

---
+++ TelegramBotProject1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TelegramBotProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
