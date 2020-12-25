2463ccc61ec0efeda6d1534dadd561c6b39e0284
Leunama-g <47783627+Leunama-g@users.noreply.github.com>
2020-12-26 01:15:10 +0300

    fixed some bugs in the game interface

--- TelegramBotProject1/Program.cs
+++ TelegramBotProject1/Program.cs
@@ -8,6 +8,8 @@ namespace TelegramBotProject1
 {
     class Program
     {
+        //if true bot has started if otherwise false
+        static bool start = false;
         // the variable holds the generated number
         // *** it's a string so that it can store the numbers like 0123 ***
         static string Gen_num;
@@ -15,8 +17,7 @@ namespace TelegramBotProject1
         static TelegramBotClient bot = new TelegramBotClient("1487222844:AAFs5T_Wl_xiLkvkxPMAQCXoTmD7caA2pjc");
 
         //text for the main menu keyboard
-        static string btn_txt1 = "Start Playing!";
-        static string btn_txt2 = "Change In Game UserName";
+        static string btn_txt1 = "Start Playing!";        
         static string btn_txt3 = "Highscore Board";
         static string btn_txt4 = "Help";
         static string btn_txt5 = "About";
@@ -24,11 +25,7 @@ namespace TelegramBotProject1
         //true if a game is being played false otherwise
         static bool playing = false;        
 
-        //keyboard buttons for the game 
-        static KeyboardButton[][] button = { new KeyboardButton[3], new KeyboardButton[3], new KeyboardButton[3], new KeyboardButton[3], new KeyboardButton[1] };
-
-        //Defining Keyboard that contains the buttons above
-        static ReplyKeyboardMarkup keyboard = new ReplyKeyboardMarkup();
+        
 
         //holds the id of the last send message by the bot either for deleting or editing the message 
         static int previous_msgID, markup_msgID;
@@ -68,15 +65,35 @@ namespace TelegramBotProject1
         {
             if (!playing)
             {
-                if (e.Message.Text == "/start")
+                if (start)
                 {
-                    Welcome(e);
-                    
+                    if (e.Message.Text == btn_txt1)
+                    {
+                        StartGame(e);
+                    }
+                    else
+                    {
+                        //propmpt the user to use the keyboard
+                        //delets the users text
+                        bot.DeleteMessageAsync(e.Message.Chat.Id, e.Message.MessageId);
+                    }
                 }
-                else if (e.Message.Text == btn_txt1)
+                else
                 {
-                    StartGame(e);
-                }
+                    Console.WriteLine("here");
+                    if (e.Message.Text == "/start")
+                    {
+                        Console.WriteLine("here");
+                        Welcome(e);
+                        start = true;
+                    }
+                    else
+                    {
+                        //propmpt the user to type /start
+                        //delets the users text
+                        bot.DeleteMessageAsync(e.Message.Chat.Id, e.Message.MessageId);
+                    }
+                }     
             }            
             else
             {
@@ -120,10 +137,9 @@ namespace TelegramBotProject1
                 {
                     Guess(e);
                 }
-                else if (e.Message.Text == "Quit")
+                else if (e.Message.Text == "Quit" || e.Message.Text == "Go Back To The Main Menu")
                 {
-                    playing = false;
-                    Welcome(e);
+                    refresh(e);
                 }
                 else if (e.Message.Text == "<--")
                 {
@@ -136,20 +152,53 @@ namespace TelegramBotProject1
 
                         int id = IDScore((digits[0].ToString() + digits[1].ToString() + digits[2].ToString() + digits[3].ToString()));
                         int pos = PosScore((digits[0].ToString() + digits[1].ToString() + digits[2].ToString() + digits[3].ToString()));
+                        
+                        //adding the idscore and the posscore to the game interface string
                         gameMsg_string +=" |  " + id.ToString() + "        " + pos.ToString() + "\n" + (++moves).ToString() + ". _ _ _ _";
+
+                        //reseting the digit array for the second guess
                         digits[0] = -1;
                         digits[1] = -1;
                         digits[2] = -1;
                         digits[3] = -1;
+
                         No_ofDigits = 0;
+
+                        //delets the users text
                         bot.DeleteMessageAsync(e.Message.Chat.Id, e.Message.MessageId);
-                        Message m = bot.EditMessageTextAsync(
+
+                        //sends the game interface to the user with the scores
+                        bot.EditMessageTextAsync(
                             chatId: e.Message.Chat,
                             messageId: previous_msgID,
                             text: gameMsg_string,
                             parseMode: ParseMode.Html
-                        ).Result;
+                        );
 
+                        //if the pos is equa to 4 the user has won
+                        //this sends the congratulations message 
+                        if (pos == 4)
+                        {
+                            bot.DeleteMessageAsync(e.Message.Chat.Id, markup_msgID);
+                            KeyboardButton button = new KeyboardButton("Go Back To The Main Menu");
+                            
+                            //Defining the Keyboard that contains the buttons above
+                            ReplyKeyboardMarkup keyboard = new ReplyKeyboardMarkup(button);
+
+                            //adjusts the size of the custom keyboard to what is only is needed
+                            keyboard.ResizeKeyboard = true;                            
+
+                            Message Keyboard_msg = bot.SendTextMessageAsync(
+                            chatId: e.Message.Chat,
+                            text: "Congratulations!!! \n\n You have guessed the number in " + (moves - 1).ToString() + " tries. \n\n to go back to the main menu press the button below",
+                            parseMode: ParseMode.Html,
+                            disableNotification: true,
+                            replyMarkup: keyboard
+                            ).Result;
+
+                            //stors the id of the above message so that it can be deleted when going back to the menu
+                            markup_msgID = Keyboard_msg.MessageId;
+                        }
                     }
                     else
                     {
@@ -159,6 +208,9 @@ namespace TelegramBotProject1
                 }
                 else
                 {
+                    //delets the users text
+                    bot.DeleteMessageAsync(e.Message.Chat.Id, e.Message.MessageId);
+
                     //send notification to the user that they should use the keyboard and also delete the message they sent
                 }
 
@@ -170,6 +222,22 @@ namespace TelegramBotProject1
 
         }
 
+        //removes all the data from a previous game so that a new one can be started 
+        static void refresh(Telegram.Bot.Args.MessageEventArgs e)
+        {
+            digits[0] = -1;
+            digits[1] = -1;
+            digits[2] = -1;
+            digits[3] = -1;
+            gameMsg_string = "You can start guessing using the <b>keyboard</b> below\n\n                    ID        POS \n1. _ _ _ _";
+            No_ofDigits = 0;
+            moves = 1;
+            playing = false;
+            bot.DeleteMessageAsync(e.Message.Chat.Id, previous_msgID);
+            bot.DeleteMessageAsync(e.Message.Chat.Id, markup_msgID);
+            Welcome(e);
+        }
+
         //function for the start game message
         static void StartGame(Telegram.Bot.Args.MessageEventArgs e)
         {         
@@ -177,6 +245,11 @@ namespace TelegramBotProject1
             bot.DeleteMessageAsync(e.Message.Chat.Id, e.Message.MessageId);
             bot.DeleteMessageAsync(e.Message.Chat.Id, markup_msgID);
 
+            //keyboard buttons for the game 
+            KeyboardButton[][] button = { new KeyboardButton[3], new KeyboardButton[3], new KeyboardButton[3], new KeyboardButton[3], new KeyboardButton[1] };
+
+            //Defining Keyboard that contains the buttons above
+            ReplyKeyboardMarkup keyboard = new ReplyKeyboardMarkup();
 
             // Defining the Keyboard buttons for the game
 
@@ -205,6 +278,7 @@ namespace TelegramBotProject1
             playing = true;
             GenerateNumber();
 
+            //sends the keyboard for the game and also sends a text message for good luck
             Message Keyboard_msg = bot.SendTextMessageAsync(
                 chatId: e.Message.Chat,
                 text: "<b>Game Started!</b> \n\nGood Luck!!!",
@@ -212,74 +286,92 @@ namespace TelegramBotProject1
                 disableNotification: true,
                 replyMarkup: keyboard
                 ).Result;
+            
+            //storing the id of the above message  for deletion 
+            markup_msgID = Keyboard_msg.MessageId;
 
-            Console.WriteLine(previous_msgID);
+            //edits the welcome message to make it look like the interface of the game
             Message m= bot.EditMessageTextAsync(
                     chatId: e.Message.Chat,
                     messageId: previous_msgID,
                     text: gameMsg_string,
                     parseMode: ParseMode.Html
                     ).Result;
-            Console.WriteLine(m.Text);
+            
         }
 
         //the function edits the game text to add the number that was pressed by the user
         static void Guess(Telegram.Bot.Args.MessageEventArgs e)
         {
+            //delets the text sent by the user 
             bot.DeleteMessageAsync(e.Message.Chat.Id, e.Message.MessageId);
-            if (No_ofDigits < 4)
+
+            // removes the last 8 charactes of the string so that the nuw number that the user enters can be added 
+            gameMsg_string = gameMsg_string.Substring(0, gameMsg_string.Length - 8);
+
+            if (e.Message.Text == "<--")
             {
-                gameMsg_string = gameMsg_string.Substring(0, gameMsg_string.Length - 8);
-                if (e.Message.Text == "<--")
+                //if the user hasnt entered any digits no nee to run this code as there aren't any numbers to remove
+                if (No_ofDigits > 0)
                 {
-                    if (No_ofDigits > 0)
-                    {
-                        digits[No_ofDigits - 1] = -1;
-                        No_ofDigits--;
-                    }
+                    //changes the number entered previously to -1 so that the user can enter another number in place of the number he added before
+                    digits[No_ofDigits - 1] = -1;
+
+                    //number of digits entered by the user is decreased 
+                    No_ofDigits--;
                 }
-                else
+            }
+            else
+            {
+                //checks if the user enters more than 4 digits
+                if (No_ofDigits < 4)
                 {
                     if (!IfRepeated(int.Parse(e.Message.Text)))
                     {
+                        //adds the number entered by the user to the digits array
+                        //later it will be added to the game interface
                         digits[No_ofDigits] = int.Parse(e.Message.Text);
+
                         No_ofDigits++;
                     }
                     else
                     {
                         //notify the user that they cant add the same number twice!!!!
                     }
-
                 }
-
-                gameMsg_string += func();
-
-
-                try
+                else
                 {
-                    Message m = bot.EditMessageTextAsync(
-                    chatId: e.Message.Chat,
-                    messageId: previous_msgID,
-                    text: gameMsg_string,
-                    parseMode: ParseMode.Html
-                    ).Result;
-                }
-                catch (AggregateException)
-                {
-
+                    //notify the user that no more than 4 digits can be entered 
                 }
             }
-            else
+
+            
+            //adds the digits to the game interface string 
+            gameMsg_string += func();
+
+            //sends the game interface string to the user
+            try
             {
-                //notify the user that no more than 4 digits can be entered 
+                Message m = bot.EditMessageTextAsync(
+                chatId: e.Message.Chat,
+                messageId: previous_msgID,
+                text: gameMsg_string,
+                parseMode: ParseMode.Html
+                ).Result;
+            }
+            catch (AggregateException)
+            {
+
             }
 
         }
 
+        //checks if the user entered a number twice 
         static bool IfRepeated(int x)
         {           
             for(int i=0; i<4; i++)
             {
+                //checks for -1 to not do more loops that it has to since -1 means the user hasent entered a digit in that row of the array
                 if (digits[i] != -1)
                 {
                     if (digits[i] == x)
@@ -293,6 +385,8 @@ namespace TelegramBotProject1
             return false;
         }
 
+        // generates the the string that is outputted when the user enters a number 
+        // also works if the user uses the backspace aka '<--' it just replaces the last number with _
         static string func()
         {
             string x="";
@@ -316,11 +410,10 @@ namespace TelegramBotProject1
         static void Welcome(Telegram.Bot.Args.MessageEventArgs e)
         {
             // Defining the Keyboard buttons for the main menu
-            KeyboardButton[][] button = { new KeyboardButton[1], new KeyboardButton[2], new KeyboardButton[2] };
+            KeyboardButton[][] button = { new KeyboardButton[1], new KeyboardButton[1], new KeyboardButton[2] };
 
-            button[0][0] = new KeyboardButton(btn_txt1);
-            button[1][0] = new KeyboardButton(btn_txt2);
-            button[1][1] = new KeyboardButton(btn_txt3);
+            button[0][0] = new KeyboardButton(btn_txt1);            
+            button[1][0] = new KeyboardButton(btn_txt3);
             button[2][0] = new KeyboardButton(btn_txt4);
             button[2][1] = new KeyboardButton(btn_txt5);
 
