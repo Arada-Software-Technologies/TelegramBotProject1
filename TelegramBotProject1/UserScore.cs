f1b5ea7a642d6eea4e2e076fd910a5909004c22f
Leunama-g <47783627+Leunama-g@users.noreply.github.com>
2020-12-26 16:49:20 +0300

    test 2

--- TelegramBotProject1/UserScore.cs
+++ TelegramBotProject1/UserScore.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
+
 
 namespace TelegramBotProject1
 {
@@ -8,17 +10,32 @@ namespace TelegramBotProject1
     {
         string username;
         int moves;
-
-        bool RegisterScore()
+        
+        public void RegisterScore()
         {
-            //if the score qualifies in to the highscore table the value will be true otherwise false
-            bool Qual_HS = false;
-            int x;
+            /*using (SqlConnection conn = new SqlConnection("Server= localhost; Database= EdirManagement_system; Integrated Security=True;"))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("spMember", conn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                // cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@userid", userid);
+                cmd.Parameters.AddWithValue("@FirstName", Fname);
+                cmd.Parameters.AddWithValue("@LastName", Lname);
+                cmd.Parameters.AddWithValue("@Phone", phoneNum);
+                cmd.Parameters.AddWithValue("@Address", houseNo);
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.Parameters.AddWithValue("@Gender", gender);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data inserted Succesfully");
+
+            }*/
 
 
 
 
-            return Qual_HS;
+
+
         }
     }
 }
